3464e1be2e93a0fb093a5d674bac2d5a3129a547
hahazhu <zzzzzmmmmm86@126.com>
2021-10-14 18:04:31 +0800

    补上注解...

--- consumer1/src/main/java/com/example/consumer1/Consumer1Application.java
+++ consumer1/src/main/java/com/example/consumer1/Consumer1Application.java
@@ -4,15 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
-
+@RestController
+@EnableEurekaClient
 @SpringBootApplication
 public class Consumer1Application {
     @Autowired
